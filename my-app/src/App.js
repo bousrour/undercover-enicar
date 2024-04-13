a63d9e8a62660d582be904ea4a8f66152d5fdb24
essra <essraouhaibi@gmail.com>
2024-04-13 23:53:01 +0100

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import LandingPage from './entre';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    
+   
+      <LandingPage/>
+   
+  );
+}
+
+export default App;
